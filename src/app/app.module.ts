53eb1e48999d0307399662d5a88ed3ed7b284508
matt-k1998 <matt.kolakowski@hotmail.com>
2022-12-19 17:29:35 +0000

    Angular Hangman

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HangmanComponent } from './components/hangman/hangman.component';
+import { HangmanDisplayComponent } from './components/hangman-display/hangman-display.component';
+import { HangmanKeyboardComponent } from './components/hangman-keyboard/hangman-keyboard.component';
+import { HangmanQuestionComponent } from './components/hangman-question/hangman-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HangmanComponent,
+    HangmanDisplayComponent,
+    HangmanKeyboardComponent,
+    HangmanQuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
